a086e654512d6081c06c3b9c50f76dee16b2c336
PauluxDelux <paul.bavazzano@gmail.com>
2017-01-09 09:05:55 +0100

    Update Aventurier.java

--- model/aventuriers/Aventurier.java
+++ model/aventuriers/Aventurier.java
@@ -1,4 +1,5 @@
 package model.aventuriers;
+package 
 
 import model.ObjetIdentifie;
 
@@ -7,4 +8,7 @@ import model.ObjetIdentifie;
  * @author IUT2-Dept Info
  */
 public abstract class Aventurier extends ObjetIdentifie {
+  
+  private nbActions : float;
+  private role : Pion;
 }

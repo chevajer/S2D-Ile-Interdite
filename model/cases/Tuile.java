12e685ab162e7ac50a35a24e537ee3efbfa29028
PauluxDelux <paul.bavazzano@gmail.com>
2017-01-09 10:14:28 +0100

    Update Tuile.java

--- model/cases/Tuile.java
+++ model/cases/Tuile.java
@@ -1,10 +1,13 @@
 package model.cases;
 
 import model.ObjetIdentifie;
-
+import util.EtatTuile;
 /**
  *
  * @author IUT2-Dept Info
  */
 public class Tuile extends ObjetIdentifie {
+  
+  private etatActuel : EtatTuile;
+  
 }
